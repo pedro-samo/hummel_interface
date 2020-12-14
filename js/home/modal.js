d8d5a3ce93d07845f338f1e997536879e0117f0c
Pedro Morais <pedro.s.morais@gmail.com>
2020-12-14 09:11:41 -0300

    hedader

--- js/home/modal.js
+++
@@ -1,9 +0,0 @@
-{
-function modalEntrar() {
-    let modalContainer = document.getElementById('modalContainerAge').classList.add('fechar');
-}
-
-function modalFechar() {
-    window.location.href = "http://www.google.com.br/";
-}
-}
