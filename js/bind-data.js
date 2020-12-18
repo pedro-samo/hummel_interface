caf358056e85ab75a5c89d925b48f53a237e9ce1
Pedro Morais <pedro.s.morais@gmail.com>
2020-12-18 15:34:06 -0300

    project end

--- js/bind-data.js
+++ js/bind-data.js
@@ -10,9 +10,26 @@ function setSecondImage(src) {
   secondImage.src = src;
 };
 
+function thumbImage1(src) {
+  const thumb1 = document.querySelector('#thumb1');
+  if (!thumb1) return;
+  thumb1.src = src;
+};
+
+function thumbImage2(src) {
+  const thumb2 = document.querySelector('#thumb2');
+  if (!thumb2) return;
+  thumb2.src = src;
+};
+
+function thumbImage3(src) {
+  const thumb3 = document.querySelector('#thumb3');
+  if (!thumb3) return;
+  thumb3.src = src;
+};
+
 async function setProductData() {
   const title = document.querySelector("#title");
-  //const bestprie = document.querySelector('#bestprice');
   const sku = document.querySelector('#productid');
   const description = document.querySelectorAll('#productdescription');
   const genero = document.querySelector('#productgenero');
@@ -35,7 +52,10 @@ async function setProductData() {
 
   setMainImage(mainProductInfo.items[0].images[0].imageUrl);
   setSecondImage(mainProductInfo.items[0].images[1].imageUrl);
-  //bestPrice.innerText = mainProductInfo.
+  thumbImage1(mainProductInfo.items[1].images[0].imageUrl);
+  thumbImage2(mainProductInfo.items[2].images[0].imageUrl);
+  thumbImage3(mainProductInfo.items[3].images[0].imageUrl);
+
   sku.innerText = mainProductInfo.productReference;
 
   description.forEach(function(item) {
